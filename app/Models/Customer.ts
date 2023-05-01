50d39f3a3085d4280e0c0973ca4a5f51b0791a43
keenplify <keenplify>
2023-05-02 00:53:38 +0800

    FIX - customer

--- app/Models/Customer.ts
+++ app/Models/Customer.ts
@@ -1,10 +1,8 @@
-import { BelongsTo, HasOne, beforeSave, belongsTo, column, hasOne } from '@ioc:Adonis/Lucid/Orm'
-import { booleanColumn } from 'App/Core/Decorators/Boolean'
+import { HasOne, beforeSave, column, hasOne } from '@ioc:Adonis/Lucid/Orm'
 import { getRandomNumber } from 'App/Core/Helpers/Random'
 import Model from 'App/Core/Models/Model'
 import { CustomerType } from 'App/Enums/CustomerType'
 import Booking from 'App/Models/Booking'
-import User from 'App/Models/User'
 import { DateTime } from 'luxon'
 export default class Customer extends Model {
   @column()
